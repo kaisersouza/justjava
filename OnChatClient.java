7fb062ccff866483b6a340c49476ad1f15d7adca
Pasha <helloitspasha@hotmail.com>
2019-12-12 21:16:45 +0300

    Update OnChatClient.java
    dataoutputstream was changed to outputstreamwriter

--- OnChatClient.java
+++ OnChatClient.java
@@ -1,21 +1,21 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 public class OnChatClient {
 
     public static void main (String[] args) throws IOException {
         Socket clientSocket = new Socket("127.0.0.1", 9000);
-        DataOutputStream outd = new DataOutputStream(clientSocket.getOutputStream());
-        DataInputStream ind = new DataInputStream(clientSocket.getInputStream());
-        System.out.println("Server: " + ind.readUTF());
-        Scanner sc = new Scanner(System.in);
-        String outmessage = sc.nextLine();
-        outd.writeUTF(outmessage);
-        System.out.println("Me: " + outmessage);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+        writer.write("My name is Polina");
+        writer.flush();
+        String input = reader.readLine();
+        System.out.println(input);
+
         clientSocket.close();
-        outd.close();
-        ind.close();
+        writer.close();
+        reader.close();
     }
 
     }
